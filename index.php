227b92e5de47c3e0610ca236d1f2172a5ca8cde8
Seme87 <massimomonteleone@hotmail.com>
2022-12-14 17:06:11 +0100

    Aggiunge stampa degli hotels tramite ciclo foreach

--- index.php
+++ index.php
@@ -48,9 +48,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- Bootstrap -->
+
     <title>Php-hotel</title>
 </head>
 <body>
-    
+    <h2>Lista Hotel</h2>
+
+    <ul>
+        <?php foreach($hotels as $hotel ){ ?>
+            <li>
+                <h3> <?php echo $hotel['name'] ?> </h3>
+                <h4> <?php echo $hotel['description'] ?> </h4>
+                <h4> <?php echo $hotel['parking'] ?> </h4>
+                <h5> <?php echo $hotel['vote'] ?> </h5>
+                <h5> <?php echo $hotel['distance_to_center'] ?> </h5>
+            </li>
+        <?php } ?>
+   </ul>
+
+
 </body>
 </html>
