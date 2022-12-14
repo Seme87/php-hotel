ec3c7d9c181f287914d801776163db1acfbbf4b0
Seme87 <massimomonteleone@hotmail.com>
2022-12-14 17:17:24 +0100

    Aggiunge tabella

--- index.php
+++ index.php
@@ -54,7 +54,10 @@
 
     <title>Php-hotel</title>
 </head>
+
 <body>
+
+
     <h2>Lista Hotel</h2>
 
     <ul>
@@ -68,7 +71,38 @@
             </li>
         <?php } ?>
    </ul>
-
+    
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col"> <?php echo  ?> </th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>        
 
 </body>
+
+
 </html>
